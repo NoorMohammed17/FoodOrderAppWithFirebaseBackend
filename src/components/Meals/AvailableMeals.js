21e0099ea3d692c043c5e5a94295ee91efabf903
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-17 13:54:31 +0530

    backend firbase url created

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,100 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import React, { useState, useEffect } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const firebase_url = "https://react-backend-81972-default-rtdb.firebaseio.com";
+
+// const DUMMY_MEALS = [
+//   {
+//     id: 'm1',
+//     name: 'Sushi',
+//     description: 'Finest fish and veggies',
+//     price: 22.99,
+//   },
+//   {
+//     id: 'm2',
+//     name: 'Schnitzel',
+//     description: 'A german specialty!',
+//     price: 16.5,
+//   },
+//   {
+//     id: 'm3',
+//     name: 'Barbecue Burger',
+//     description: 'American, raw, meaty',
+//     price: 12.99,
+//   },
+//   {
+//     id: 'm4',
+//     name: 'Green Bowl',
+//     description: 'Healthy...and green...',
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchMealsData = async () => {
+      const response = await fetch(`${firebase_url}/meals.json`);
+
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+      const responseData = await response.json();
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals)
+      setIsLoading(false)
+    };
+
+
+    // try{
+    //   fetchMealsData();
+    // }
+    // catch(error){
+    //   setIsLoading(false)
+    //   setHttpError(error.message);
+    // }
+
+    fetchMealsData().catch((error) =>{
+      setIsLoading(false)
+       setHttpError(error.message);
+    });
+    //this is the traditional way of handling error inside a promise
+  
+  }, []);
+
+
+
+  if(isLoading){
+    return(
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+  if(httpError){
+    return(
+      <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
+    )
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
